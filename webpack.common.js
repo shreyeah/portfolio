32be932f53036ae4666bdbe5db4eddbdf7d2b934
Shreya Shrivastava <shreya@byteblanket.com>
2021-04-10 19:16:42 +0530

    color theme

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
         use: ["html-loader"],
       },
       {
-        test: /\.(jpg|jpeg|png|gif|svg|pdf)$/i,
+        test: /\.(jpg|jpeg|png|gif|svg|pdf|css)$/i,
         use: [
           {
             loader: "file-loader",
